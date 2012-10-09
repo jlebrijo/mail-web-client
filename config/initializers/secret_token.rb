9032e73a66f49f92a172e334c1fe66e500c32352
Juanan Lebrijo <jlebrijo@gmail.com>
2012-10-09 19:09:15 +0200

    #96 Authenticate to the user inbox

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MailWebClient::Application.config.secret_token = 'f24ce2f4e41638d5877198524fed00de2eef7dae444471cd529405a37b5121a6b97c021b7db6e19e8290585a16d47f448c49070d2a4ac6118edf2015546a4948'
+MailWebClient::Application.config.secret_token = '47cce86ab4eb78222a9d5f104079e4a2b5e75cd8c3663d683e595859bb9304e0a6a2c93f538645d674912af70cebb129375038780ebfb4c95065b410d6765f51'
